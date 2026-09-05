d711b2071f4e50112a39381664b548ba6308102e
AlanRankingPoke <AlanRankingPoke@users.noreply.github.com>
2026-09-05 14:03:39 -0300

    Configura GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/pokemon-mundo-perfeito-owlbear/",
+
   server: {
     cors: {
       origin: "https://www.owlbear.rodeo",
